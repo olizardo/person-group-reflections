698f2abef8a6ae2a53419a8762fe061ac01e05f9
Omar Lizardo <olizardo@soc.ucla.edu>
2025-07-10 08:48:05 -0700

    ref

--- reflections.R
+++ reflections.R
@@ -2,8 +2,8 @@ reflections <- function(x, iter = 20) { #x is a matrix with people as rows and g
     z <- t(x)
     p <- nrow(x)
     g <- ncol(x)
-    p.c <- matrix(0, p, iter) #initialize person centralities
-    g.c <- matrix(0, g, iter) #initialize group centralities trajectory matrix
+    p.c <- matrix(0, p, iter) #initialize person reflections trajectory matrix
+    g.c <- matrix(0, g, iter) #initialize group reflections trajectory matrix
     rownames(p.c) <- rownames(x)
     rownames(g.c) <- colnames(x)
     colnames(p.c) <- paste("Ref_", c(1:iter), sep = "")
@@ -12,14 +12,13 @@ reflections <- function(x, iter = 20) { #x is a matrix with people as rows and g
     g.c[, 1] <- colSums(x) #group degree centrality 
     k <- 1 #initializing counter
     while (k < iter) {
-        m <- k + 1
-        p.c[, m] <- (x %*% g.c[, k]) * p.c[, 1]^-1 #assign person average centrality of groups they belong to
-        g.c[, m] <- (z %*% p.c[, k]) * g.c[, 1]^-1 #assign group average centrality of people in the group
+        p.c[, k + 1] <- (x %*% g.c[, k]) * p.c[, 1]^-1 #assign person average reflection of groups they belong to
+        g.c[, k + 1] <- (z %*% p.c[, k]) * g.c[, 1]^-1 #assign group average reflection of people in the group
         k <- k + 1 #increase counter
         } #end while loop
-    p.s <- scale(p.c)
-    g.s <- scale(g.c)
-    p.r <- apply(p.s, 2, rank)
-    g.r <- apply(g.s, 2, rank)
+    p.s <- scale(p.c) #scaling person reflections
+    g.s <- scale(g.c) #scaling group reflections
+    p.r <- apply(p.s, 2, rank) #ranking person reflections
+    g.r <- apply(g.s, 2, rank) #ranking group reflections
     return(list(p.c = p.c, g.c = g.c, p.s = p.s, g.s = g.s, p.r = p.r, g.r = g.r))
     } #end function
